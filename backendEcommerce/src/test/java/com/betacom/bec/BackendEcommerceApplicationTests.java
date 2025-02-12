40a332287fcdf9ac78fe84e50b403f5fae8b41f5
salva <salva@andrea.salvati1992-himalayas.nord>
2025-02-12 20:19:18 +0100

    commit

--- backendEcommerce/src/test/java/com/betacom/bec/BackendEcommerceApplicationTests.java
+++ backendEcommerce/src/test/java/com/betacom/bec/BackendEcommerceApplicationTests.java
@@ -1,7 +1,21 @@
 package com.betacom.bec;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+
+@Suite
+@SelectClasses({
+	ProdottoControllerTest.class,
+	ProdottoTest.class,
+	CarrelloControllerTest.class,
+	CarrelloTest.class,
+	RecensioneControllerTest.class,
+	RecensioneTest.class,
+	UtenteControllerTest.class,
+	UtenteTest.class
+})
 
 @SpringBootTest
 class BackendEcommerceApplicationTests {
